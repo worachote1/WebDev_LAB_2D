7439a295ba0c2fc3b1eaeef04733286cbc78018c
worachote1 <timeworachote2002@gmail.com>
2023-02-16 09:50:01 +0700

    update usr

--- Topic_DOM/worker.js
+++ Topic_DOM/worker.js
@@ -4,18 +4,20 @@ self.onmessage = (message) => {
         "bg" : ['#DFFF00', '#FFBF00', '#FF7F50','#DE3163','#9FE2BF','#40E0D0'],
         "text" : ['#6495ED','#6495ED','#454545','#800080','#008080','#000080']
     }
-    Idx = 0
+    let IdxBgColor = 0
+    let IdxTextColor = 0
     postMessage({
         "date_prn" : new Date(),
-        "color_bg_btn" : color_data.bg[Idx % color_data.bg.length],
-        "color_text_btn" : color_data.text[Idx % color_data.text.length]
+        "color_bg_btn" : color_data.bg[IdxBgColor],
+        "color_text_btn" : color_data.text[IdxTextColor]
     })
     setInterval(() => {
-        Idx += 1
+        IdxBgColor = (IdxBgColor + 1) % color_data.bg.length
+        IdxTextColor = (IdxTextColor + 1) % color_data.text.length
         postMessage({
             "date_prn" : new Date(),
-            "color_bg_btn" : color_data.bg[Idx % color_data.bg.length],
-            "color_text_btn" : color_data.text[Idx % color_data.text.length]
+            "color_bg_btn" : color_data.bg[IdxBgColor],
+            "color_text_btn" : color_data.text[IdxTextColor]
         })
     }, 1000)
 }
